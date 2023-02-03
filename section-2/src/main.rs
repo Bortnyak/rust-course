0b749497f722418b21b40b12f50a52c504663ecf
Volodymyr Bortniak <volodymyr.bortniak@qpercom.ie>
2023-02-03 17:04:44 +0200

    playing with DS

---
+++ section-2/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    println!("Variables");
+
+    let mut x = 5;
+    x = 6;
+    let _y = x; // hm, interestin
+    println!("x = {}", x);
+
+    // Tuple
+    let tup = (300, "Hello", true);
+    let (x, y, z) = tup;
+    println!("x = {}", x);
+    println!("y = {}", y);
+    println!("z = {}", z);
+
+    //Array
+    let array = [1, 2, 3];
+    println!("Array[0] = {}", array[0]);
+    let array2: [i32; 3] = [4, 5, 6];
+    println!("Array = {}", array2[0]);
+
+    // Vector
+    let mut nums = vec![1, 2, 3];
+    nums.push(4);
+    println!("Array = {:?}", nums);
+    let popped = nums.pop();
+    println!("popped = {:?}", popped);
+    nums.reverse();
+    println!("vec = {:?}", nums);
+
+    // Slice
+    let sv: &[i32] = &nums[0..2];
+    println!("slice sv = {:?}", sv);
+
+    //Strings
+    let myString = String::from("My string");
+    println!("myString = {}", myString);
+}
