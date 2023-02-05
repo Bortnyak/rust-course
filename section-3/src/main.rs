6b7dd8ccbde3955bd2293f592494e36a50b61b3e
Volodymyr Bortniak <volodymyr.bortniak@qpercom.ie>
2023-02-05 17:27:57 +0200

    section 3 - assignment

--- section-3/src/main.rs
+++ section-3/src/main.rs
@@ -1,3 +1,5 @@
+use std::{ptr::eq, vec};
+
 fn main() {
     // ownership
     // let my_var = 1; // on the stack;
@@ -14,4 +16,24 @@ fn main() {
     my_int = 5;
     println!("{}", my_int);
     println!("{}", my_int_2);
+
+    // vector task
+    let mut vec_vals = vec![1, 3, 5, 7];
+    let is_eq = is_first_eq_one(&vec_vals);
+    println!("Is first equal to one = {:?}", is_eq);
+
+    add_15_to_vec(&mut vec_vals);
+    println!("vecotr after mutation = {:?}", vec_vals);
+}
+
+fn is_first_eq_one(val: &Vec<i32>) -> bool {
+    let is_equal_to_one = &val[0] == &1;
+    if !is_equal_to_one {
+        return false;
+    }
+    true
+}
+
+fn add_15_to_vec(val: &mut Vec<i32>) {
+    val.push(15)
 }
